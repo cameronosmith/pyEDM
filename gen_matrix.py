c4348aa9e112ea936e0178cc296296241ca476b6
cameronosmith <cos008@ucsd.edu>
2019-07-31 10:33:20 -0700

    azure pipeline testing

--- gen_matrix.py
+++ gen_matrix.py
@@ -1,10 +1,26 @@
-platforms       = [['Linux','ubuntu-16.04'],['Windows','vs2017-win2016'],
-                    ['Mac','macos-10.13']]
-python_versions = ['2.7','3.4','3.5','3.6','3.7']
+class build_job:
+    def __init__(self,name, platform_id, python_versions ) :
+        self.name = name
+        self.platform_id = platform_id
+        self.python_versions = python_versions
 
-for platform_name, platform in platforms:
-    for python_version in python_versions:
-        print("    {}_{}:".format(platform_name,python_version))
-        print("      vmImage: '{}'".format(platform))
-        print("      build_name: '{}_{}_EDM'".format(platform_name,platform))
+windows = build_job( "Windows",
+                     "vs2017-win2016",
+                     ['3.5.4','3.6.8','3.7.4'] )
+mac     = build_job( "MacOS",
+                     "macos-10.13",
+                     ['2.7.16','3.4.10','3.5.7','3.6.9','3.7.4'] )
+linux   = build_job( "Linux",
+                     "ubuntu-latest",
+                     ['2.7.16','3.4.10','3.5.7','3.6.9','3.7.4'] )
+build_jobs = [windows,mac,linux]
+
+for build_job in build_jobs:
+        platform_name = build_job.name
+        platform      = build_job.platform_id
+        for python_version in build_job.python_versions:
+            print("    {}_{}:".format(platform_name,python_version))
+            print("      vmImage: '{}'".format(platform))
+            print("      build_name: '{}_{}_EDM'".format(platform_name,python_version))
+            print("      python.version: '{}'".format(python_version))
         
