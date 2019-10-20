1796080d20b64fbd52c6c59bad51619ea3c70d40
cameronosmith <cos008@ucsd.edu>
2019-10-19 23:38:04 -0700

    apping

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -7,7 +7,8 @@ set -e -x
 git clone https://github.com/eigenteam/eigen-git-mirror eigen_git
 mv eigen_git/Eigen .
 
-git clone https://github.com/sugiharalab/cppEDM
+ls
+
 make -C cppEDM/src clean
 make -C cppEDM/src 
 
