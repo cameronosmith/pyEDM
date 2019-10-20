824125875ee6b202da82216705bc4054f2b06be0
cameronosmith <cos008@ucsd.edu>
2019-10-20 00:08:55 -0700

    apping

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -12,6 +12,11 @@ make -C io/cppEDM/src
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
+    ls ${PYBIN}
+    if [ ${PYBIN} -eq "cp27-cp27m" ]
+    then
+        continue  ### resumes iteration of an enclosing for loop ###
+    fi
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
