c33f3d92740ce301edc668d14943501fadb57f75
cameronosmith <cos008@ucsd.edu>
2019-11-08 15:41:43 -0800

    apping

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ Extension_modules = [
         
         language     = 'c++',
         library_dirs = [ EDM_Lib_Path, '/usr/lib/' ],
-        libraries    = ['lapack','EDM'],
+        libraries    = ['blas','lapacke','lapack','EDM'],
     ),
 ]
 
