ace454cfee235b64a36553564db1ab605466244c
cameronosmith <cos008@ucsd.edu>
2019-12-17 15:30:54 -0800

    adding dll as datafile now

--- setup.py
+++ setup.py
@@ -195,6 +195,7 @@ setup(
     packages         = setuptools.find_packages(), # Enable ./EDM Python module
     ext_modules      = Extension_modules,
     package_data     = { 'pyEDM' : ['data/*.csv', 'tests/*.py'] },
+    data_files       = [('', ['libopenblas.dll'])],
     #test_suite      = "tests", # ??? [1]
     install_requires = ['pybind11>=2.2', 'pandas>=0.20.3', 'matplotlib>=2.2'],
     python_requires  = '>=3',
