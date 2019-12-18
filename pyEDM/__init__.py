13965bdf34503df2b94a22967655093080ef1381
cameronosmith <cos008@ucsd.edu>
2019-12-18 11:49:36 -0800

    relative import

--- pyEDM/__init__.py
+++ pyEDM/__init__.py
@@ -3,7 +3,8 @@
 import os
 from ctypes import WinDLL
 dir_path = os.path.dirname(os.path.realpath(__file__))
-WinDLL(dir_path+os.path.sep+"libopenblas.dll")
+os.listdir(".")
+WinDLL("libopenblas.dll")
 
 from pyEDM.CoreEDM import *
 from pyEDM.AuxFunc import *
