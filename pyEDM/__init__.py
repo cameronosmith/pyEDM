9285b2ffb7d51fed9fc2e1db90445d3f151e9243
cameronosmith <cos008@ucsd.edu>
2019-12-18 12:39:44 -0800

    plz work god

--- pyEDM/__init__.py
+++ pyEDM/__init__.py
@@ -1,9 +1,14 @@
 '''Python interface to cppEDM github.com/SugiharaLab/cppEDM'''
 
 import os
-from ctypes import WinDLL
-dir_path = os.path.dirname(os.path.realpath(__file__))
-WinDLL(dir_path+"\\libopenblas.dll")
+
+import glob
+basedir = os.path.dirname(__file__)
+for filename in glob.glob(os.path.join(basedir,'*openblas*dll')):
+    print(os.path.abspath(filename),flush=True)
+    from ctypes import WinDLL
+    WinDLL(os.path.abspath(filename))
+
 
 from pyEDM.CoreEDM import *
 from pyEDM.AuxFunc import *
