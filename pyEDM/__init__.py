01213fd152c0f3a28985ec64a79d3d7898b4be9c
cameronosmith <cos008@ucsd.edu>
2019-12-18 17:13:28 -0800

    more dependecnies includasdsed

--- pyEDM/__init__.py
+++ pyEDM/__init__.py
@@ -1,14 +1,12 @@
 '''Python interface to cppEDM github.com/SugiharaLab/cppEDM'''
 
 import os
-
-import glob
-basedir = os.path.dirname(__file__)
-for filename in glob.glob(os.path.join(basedir,'*openblas*dll')):
-    print(os.path.abspath(filename),flush=True)
-    from ctypes import WinDLL
-    WinDLL(os.path.abspath(filename))
-
+from ctypes import WinDLL
+dir_path = os.path.dirname(os.path.realpath(__file__))
+WinDLL(dir_path + os.path.sep + "libgcc_s_seh-1.dll")
+WinDLL(dir_path + os.path.sep + "libquadmath-0.dll")
+WinDLL(dir_path + os.path.sep + "libgfortran-3.dll")
+WinDLL(dir_path + os.path.sep + "libopenblas.dll")
 
 from pyEDM.CoreEDM import *
 from pyEDM.AuxFunc import *
