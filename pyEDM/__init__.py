bd9e3bb8b6eedbd1f576c73745dd9c4b1f2624d3
cameronosmith <cos008@ucsd.edu>
2019-12-18 17:19:55 -0800

    rip

--- pyEDM/__init__.py
+++ pyEDM/__init__.py
@@ -3,9 +3,9 @@
 import os
 from ctypes import WinDLL
 dir_path = os.path.dirname(os.path.realpath(__file__))
-WinDLL(dir_path + os.path.sep + "libgcc_s_seh-1.dll")
 WinDLL(dir_path + os.path.sep + "libquadmath-0.dll")
 WinDLL(dir_path + os.path.sep + "libgfortran-3.dll")
+WinDLL(dir_path + os.path.sep + "libgcc_s_seh-1.dll")
 WinDLL(dir_path + os.path.sep + "libopenblas.dll")
 
 from pyEDM.CoreEDM import *
