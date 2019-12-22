e085516ee0bbdafc046982a599bfdb1dc45aac8d
cameronosmith <cos008@ucsd.edu>
2019-12-22 14:02:39 -0800

    nuked

--- push_script.sh
+++
@@ -1,6 +0,0 @@
-#git add .appveyor.yml 
-#git add .travis.yml
-#git add travis/build-wheels.sh
-git add azure-pipelines.yml
-git commit -m "apping"
-git push
--- run_import_test.py
+++
@@ -1,9 +0,0 @@
-from pyEDM import *
-
-"""
-a = Simplex( dataFrame = sample_data['TentMap'], 
-             columns="TentMap",target="TentMap",
-             lib = "1 100", pred="105 200", E=1 )
-"""
-
-#Examples()
