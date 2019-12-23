745e212130152d01a3bf363dd7b03f31cafe11f2
cameronosmith <cos008@ucsd.edu>
2019-12-23 15:46:32 -0800

    only 37

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,10 +5,13 @@ set -e -x
 yum install -y atlas-devel
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+
+
+PYBIN="/opt/python/cp37-cp37m/bin"
+#for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-done
+#done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
@@ -16,7 +19,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+#for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-done
+#done
