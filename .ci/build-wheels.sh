cc9af4edd222e9b0eee4833d3f20f2d389ff2295
cameronosmith <cos008@ucsd.edu>
2019-12-23 16:12:23 -0800

    now azre do ut

--- .ci/build-wheels.sh
+++ .ci/build-wheels.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 set -e -x
 
-# Collect the pythons
-pys=(/opt/python/*/bin)
-
-# Filter out Python 3.4
-pys=(${pys[@]//*27*/})
-
-make -C /io/cppEDM/src
-
-yum install -y lapack
-
-PYBIN="/opt/python/cp37-cp37m/bin"
+# Install a system package required by our library
+yum install -y lapack lapack-devel
 
 # Compile wheels
-#for PYBIN in "${pys[@]}"; do
+make -C /io/cppEDM/src
+
+
+PYBIN="/opt/python/cp37-cp37m/bin"
+#for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-    #"${PYBIN}/python" /io/setup.py bdist_wheel --dist-dir wheelhouse/
 #done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/pyEDM-*.whl; do
-    auditwheel repair --plat $PLAT "$whl" -w /io/wheelhouse/
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
 
 # Install packages and test
-#for PYBIN in "${pys[@]}"; do
-    "${PYBIN}/python" -m pip install pyEDM --user --no-index -f /io/wheelhouse
-    #echo "import pyEDM" | "${PYBIN}/python"
+#for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+    #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 #done
