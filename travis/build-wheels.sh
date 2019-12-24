67aff4af6bea9e457967761801b3c963e9ad8b62
cameronosmith <cos008@ucsd.edu>
2019-12-23 16:37:45 -0800

    confirm

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # Install a system package required by our library
-yum install -y lapack lapack-devel
+#yum install -y lapack lapack-devel
 
 # Compile wheels
 make -C /io/cppEDM/src
@@ -21,6 +21,6 @@ done
 
 # Install packages and test
 #for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+    #"${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+    #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 #done
